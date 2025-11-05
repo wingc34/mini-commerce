dcbdcd84f7b6d1c4c5d0807c468806763b723598
wingc34 <learnarr222@gmail.com>
2025-11-05 22:40:58 +0900

    feat: product card
    1. add product grid
    2. sync layout ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
 import HeroBanner from '@/components/landing/Hero-banner';
 import { FeaturedProducts } from '@/components/landing/Featured-products';
 export default async function Home() {
-
   return (
-    <div className="flex min-h-screen items-center justify-center font-san">
-      <main className="min-h-screen w-full">
-        <HeroBanner />
-        <FeaturedProducts />
-      </main>
-    </div>
+    <>
+      <HeroBanner />
+      <FeaturedProducts />
+    </>
   );
 }
