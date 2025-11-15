3269410a8a30ab57439512fba4407d084a318dc2
wingc34 <learnarr222@gmail.com>
2025-11-15 13:17:24 +0900

    feat: init stripe
    add stripe library

---
+++ src/lib/stripe.ts
@@ -0,0 +1,8 @@
+// lib/stripe.ts
+import Stripe from 'stripe';
+
+if (!process.env.STRIPE_SECRET_KEY) {
+  throw new Error('Missing STRIPE_SECRET_KEY in env');
+}
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
