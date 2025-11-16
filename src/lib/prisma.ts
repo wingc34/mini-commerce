f6b1c00816e408b00b13a98bca2211ebfe66f4a7
wingc34 <learnarr222@gmail.com>
2025-11-16 22:23:17 +0900

    feat: prisma data

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,11 @@
-import { PrismaClient } from '@/generated/prisma/client'
+import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = global as unknown as { 
-    prisma: PrismaClient
-}
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
 
-const prisma = globalForPrisma.prisma || new PrismaClient()
+const prisma = globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
-export default prisma
+export default prisma;
