6e8eac861d1ca7cb7710e6e0effb5245d98220a7
wingc34 <learnarr222@gmail.com>
2025-12-03 11:36:40 +0900

    fix: deploy
    error: Prisma Client could not locate the Query Engine for runtime "rhel-openssl-3.0.x

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,10 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  outputFileTracingIncludes: {
+    '/api/**/*': ['./node_modules/.prisma/client/**/*'],
+    '/*': ['./node_modules/.prisma/client/**/*'],
+  },
 };
 
 export default nextConfig;
